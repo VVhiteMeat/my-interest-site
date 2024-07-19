b20fabe72418acd093bd6391230108e46479cefe
VVhiteMeat <60025215+VVhiteMeat@users.noreply.github.com>
2024-07-18 23:57:32 -0700

    Update OrbitControls.js

--- OrbitControls.js
+++ OrbitControls.js
@@ -1,7 +1,7 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
 
 class OrbitControls extends THREE.EventDispatcher {
-    // Existing OrbitControls implementation
+    // Your existing OrbitControls implementation
 }
 
 export { OrbitControls };
