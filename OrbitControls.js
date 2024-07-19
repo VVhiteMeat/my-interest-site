6dcbdb855ed2494cf35fbfae81ab7327b60fd681
VVhiteMeat <60025215+VVhiteMeat@users.noreply.github.com>
2024-07-19 08:27:30 -0700

    Delete OrbitControls.js

--- OrbitControls.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
-
-class OrbitControls extends THREE.EventDispatcher {
-    // Your existing OrbitControls implementation
-}
-
-export { OrbitControls };
- 
